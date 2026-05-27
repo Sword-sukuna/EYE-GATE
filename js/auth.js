64e336df086f2d553707ee028dca67f0fa1668f1
Raul Rosa <109114162+Sword-sukuna@users.noreply.github.com>
2026-05-27 16:31:45 -0300

    fix auth

--- js/auth.js
+++ js/auth.js
@@ -24,6 +24,7 @@ function iniciarLogin(){
 
 }
 
+
 // =========================
 // 🔐 LOGIN ADMIN
 // =========================
@@ -50,6 +51,7 @@ function iniciarAdminLogin(){
 
 }
 
+
 // =========================
 // 📝 REGISTRO
 // =========================
@@ -75,6 +77,7 @@ function iniciarRegistro(){
   );
 
 }
+
 
 // =========================
 // 👤 REGISTRAR USUÁRIO
@@ -162,6 +165,7 @@ async function registrarUsuario(){
 
 }
 
+
 // =========================
 // 🚪 LOGIN USER
 // =========================
@@ -243,6 +247,7 @@ async function fazerLogin(){
   }
 
 }
+
 
 // =========================
 // 🔐 LOGIN ADMIN
@@ -374,6 +379,91 @@ function carregarUsuario(){
   }
 
 }
+
+
+// =========================
+// 📊 STATS
+// =========================
+async function carregarStats(){
+
+  // USERS
+  const {
+    data:usuarios
+  } = await supabaseClient
+
+    .from("usuarios")
+   .select("id");
+
+  // ALUNOS
+  const {
+    data:alunos
+  } = await supabaseClient
+
+    .from("alunos")
+   .select("id");
+
+  // LOGS
+  const {
+    data:logs
+  } = await supabaseClient
+
+    .from("logs")
+   .select("id");
+
+  // USERS
+  const totalUsers =
+    document.getElementById(
+      "totalUsers"
+    );
+
+  if(totalUsers){
+
+    totalUsers.innerText =
+      usuarios?.length || 0;
+
+  }
+
+  // ALUNOS
+  const totalAlunos =
+    document.getElementById(
+      "totalAlunos"
+    );
+
+  if(totalAlunos){
+
+    totalAlunos.innerText =
+      alunos?.length || 0;
+
+  }
+
+  // RECONHECIMENTOS
+  const totalReconhecimentos =
+    document.getElementById(
+      "totalReconhecimentos"
+    );
+
+  if(totalReconhecimentos){
+
+    totalReconhecimentos.innerText =
+      logs?.length || 0;
+
+  }
+
+  // LOGS
+  const totalLogs =
+    document.getElementById(
+      "totalLogs"
+    );
+
+  if(totalLogs){
+
+    totalLogs.innerText =
+      logs?.length || 0;
+
+  }
+
+}
+
 
 // =========================
 // 🚪 LOGOUT
