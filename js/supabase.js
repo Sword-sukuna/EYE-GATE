cf1f00c516c806b06b822366c4c3c789c792c2b3
Raul Rosa <109114162+Sword-sukuna@users.noreply.github.com>
2026-05-27 15:53:22 -0300

    fix supabase

--- js/supabase.js
+++ js/supabase.js
@@ -5,9 +5,9 @@ const SUPABASE_URL =
   "https://rhopvipdkeawvejztzix.supabase.co";
 
 const SUPABASE_KEY =
-  "SUA_KEY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJob3B2aXBka2Vhd3Zlanp0eml4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMTUxNDUsImV4cCI6MjA5NDY5MTE0NX0.U7NAbG461jLbeSqwkP6gecHFg1UoNDkKY4mUH29NtYA";
 
-window.supabaseClient = window.supabase?.createClient?.(
+  window.supabaseClient = window.supabase?.createClient?.(
   SUPABASE_URL,
   SUPABASE_KEY
 );
