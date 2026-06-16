804614fb87324e946d4a1f99e67d2ff44e7a9244
Raul Rosa <109114162+Sword-sukuna@users.noreply.github.com>
2026-06-16 15:10:20 -0300

    navigation

--- js/ui/navigation.js
+++ js/ui/navigation.js
@@ -39,9 +39,30 @@ function abrirPagina(id){
 
       });
 
-    document
-      .getElementById(id)
-      .classList.add("active-page");
+    const pagina =
+document.getElementById(id);
+
+console.log(
+  "Tentando abrir:",
+  id
+);
+
+console.log(
+  "Elemento:",
+  pagina
+);
+
+if(!pagina){
+  console.error(
+    "Página não encontrada:",
+    id
+  );
+  return;
+}
+
+pagina.classList.add(
+  "active-page"
+);
 
     esconderLoading();
 
