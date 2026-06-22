1b176a6eff767cff44c20ca5bab7d826c7ef5589
Raul Rosa <109114162+Sword-sukuna@users.noreply.github.com>
2026-06-22 14:41:36 -0300

    fix supabase

--- js/core/supabase.js
+++ js/core/supabase.js
@@ -1,16 +1,12 @@
 // =========================
 // ☁ SUPABASE
 // =========================
-const SUPABASE_URL =
-  "https://rhopvipdkeawvejztzix.supabase.co";
+const SUPABASE_URL = "https://rhopvipdkeawvejztzix.supabase.co";
 
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJob3B2aXBka2Vhd3Zlanp0eml4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMTUxNDUsImV4cCI6MjA5NDY5MTE0NX0.U7NAbG461jLbeSqwkP6gecHFg1UoNDkKY4mUH29NtYA";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJob3B2aXBka2Vhd3Zlanp0eml4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMTUxNDUsImV4cCI6MjA5NDY5MTE0NX0.U7NAbG461jLbeSqwkP6gecHFg1UoNDkKY4mUH29NtYA";
 
-  const supabaseClient =
-  window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-  );
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 window.supabaseClient = supabaseClient;
+
+console.log("✅ Supabase Client inicializado com sucesso");
